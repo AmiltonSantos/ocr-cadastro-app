848482e3d00cf558c62786007db0ad3ec2d8b235
Amilton Santos <a1000ton1000ton@gmail.com>
2026-03-20 14:00:13 -0300

    remove environment.ts do tracking

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    openaiApiKey: process.env['OPENAI_API_KEY'] || '',
-    googleApiKey: process.env['GOOGLE_API_KEY'] || '',
-    anthropicApiKey: process.env['ANTHROPIC_API_KEY'] || '',
-    groqApiKey: process.env['GROQ_API_KEY'] || '',
-};
